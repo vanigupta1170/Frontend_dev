4e82da7c225ad2de7805c36bf3d667bd5cd3cb6d
Shibani Gupta <shibanigupta1170@gmail.com>
2026-06-03 23:53:45 +0530

    Understood basics of loop and wrote a program to print odd numbers

---
+++ Day_03_Loops/app.js
@@ -0,0 +1,14 @@
+// for(let i=0; i<10; i++){
+//     console.log(i);
+// }
+
+//print odd numbers from 1 to 20
+for(let i=1;i<=20;i++){
+    if(i%2!=0){
+        console.log(i);
+    }
+    else{
+        console.log(i+" is even number, please skip it");
+    }
+}
+
