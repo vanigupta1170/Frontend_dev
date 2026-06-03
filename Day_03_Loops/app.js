f633db69ff285275869bf9873406901770c1df1e
Shibani Gupta <shibanigupta1170@gmail.com>
2026-06-03 23:58:20 +0530

    program to print even numbers from 2 to 20

--- Day_03_Loops/app.js
+++ Day_03_Loops/app.js
@@ -12,3 +12,12 @@ for(let i=1;i<=20;i++){
     }
 }
 
+//print even numbers from 2 to 20
+for(let i=2;i<=20;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+    else{
+        console.log(i+" is odd number, please skip it");
+    }
+}
